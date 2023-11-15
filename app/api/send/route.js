56f38f38b48271a6a167ad1f1ada8b49d1e41b80
Arlind <56867496+Lindi31@users.noreply.github.com>
2023-11-15 21:56:56 +0100

    Update route.js

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -4,6 +4,42 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const containerStyle = {
+  fontFamily: 'Arial, sans-serif',
+  maxWidth: '600px',
+  margin: '0 auto',
+  border: '1px solid #e0e0e0',
+  borderRadius: '5px',
+  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+};
+
+const headerStyle = {
+  background: '#007BFF',
+  color: '#fff',
+  padding: '20px',
+  textAlign: 'center',
+  borderTopLeftRadius: '5px',
+  borderTopRightRadius: '5px',
+};
+
+const logoStyle = {
+  maxWidth: '100px',
+};
+
+const contentStyle = {
+  padding: '20px',
+  backgroundColor: '#ffffff',
+};
+
+const footerStyle = {
+  padding: '10px',
+  fontSize: '12px',
+  textAlign: 'center',
+  background: '#f5f5f5',
+  borderBottomLeftRadius: '5px',
+  borderBottomRightRadius: '5px',
+};
+
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
@@ -15,18 +51,20 @@ export async function POST(req, res) {
       subject: subject,
       react: (
         <>
-          <div style={{ background: '#f5f5f5', padding: '20px', textAlign: 'center' }}>
-            <img src="/images/logo.png" alt="Ihr Logo" style={{ maxWidth: '200px' }} />
+        <div style={containerStyle}>
+          <div style={headerStyle}>
+            <img src="/images/logo.png" alt="AISoft" style={logoStyle} />
           </div>
-          <div style={{ maxWidth: '600px', margin: '0 auto', padding: '20px' }}>
+          <div style={contentStyle}>
             <p>Hallo {email},</p>
             <p>Danke für deine Nachricht! Wir werden uns in Kürze mit Dir in Kontakt setzen.</p>
             <p>Neue Nachricht übermittelt:</p>
             <p>{message}</p>
           </div>
-          <div style={{ background: '#f5f5f5', padding: '20px', textAlign: 'center' }}>
+          <div style={footerStyle}>
             <p>Dies ist eine automatisch generierte E-Mail. Bitte antworte nicht auf diese Nachricht.</p>
           </div>
+        </div>
         </>
       ),
     });
