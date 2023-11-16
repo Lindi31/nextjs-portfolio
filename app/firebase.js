649fb23291a555ae54859882c72aa428855c9f2f
Arlind <56867496+Lindi31@users.noreply.github.com>
2023-11-16 21:59:08 +0100

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getAnalytics, isSupported } from 'firebase/analytics';
+
 import { getAuth } from 'firebase/auth'; // Importieren Sie die Authentifizierungs-Funktionen
-import { useEffect } from 'react';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,14 +18,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-useEffect(() => {
-  isSupported().then((supported) => {
-    if (supported) {
-      // Initialisieren Sie Analytics hier, weil dies nur auf dem Client geschieht
-      getAnalytics(app);
-    }
-  });
-}, []);
 const auth = getAuth(app); // Initialisieren Sie die Authentifizierungsfunktionen
 
 export default app;
